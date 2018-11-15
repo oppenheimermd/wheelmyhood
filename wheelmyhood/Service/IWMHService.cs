e2616dac65cb4d844264f8a7c1ca99164ef80960
Michiel <michieloppenheimer@gmail.com>
2018-11-15 21:38:33 +0000

    updated controllers/ removed unecessary .js files / added helpers

--- wheelmyhood/Service/IWMHService.cs
+++ wheelmyhood/Service/IWMHService.cs
@@ -6,6 +6,7 @@ using WheelMyHood.Models;
 
 namespace WheelMyHood.Service
 {
+    // ReSharper disable once InconsistentNaming
     public interface IWMHService
     {
         //  Queries
@@ -18,6 +19,35 @@ namespace WheelMyHood.Service
         /// <returns></returns>
         Task<IEnumerable<Region>> GetRegionsAsync(int count, int skip = 0);
 
+        /// <summary>
+        /// Returns a list collection of <see cref="Region"/> for our dropdown control used
+        /// in view
+        /// </summary>
+        /// <returns></returns>
+        Task<List<Region>> GetRegionsDropList();
+
+        /// <summary>
+        /// Get a specific instance of a <see cref="Region"/> entity
+        /// </summary>
+        /// <param name="regionCode"></param>
+        /// <returns></returns>
+        Task<Region> GetRegionAsync(string regionCode);
+
+        /// <summary>
+        /// Get's pageable List of cities <see cref="City"/> includes <see cref="Region"/>
+        /// </summary>
+        /// <param name="count"></param>
+        /// <param name="skip"></param>
+        /// <returns></returns>
+        Task<IEnumerable<City>> GetCitiesAsync(int count, int skip = 0);
+
+        /// <summary>
+        /// Returns a <see cref="City"/> entity
+        /// </summary>
+        /// <param name="cityCode"></param>
+        /// <returns></returns>
+        Task<City> GetCityAsync(string cityCode);
+
 
         //  Create / Update
 
@@ -27,5 +57,26 @@ namespace WheelMyHood.Service
         /// <param name="region"></param>
         /// <returns></returns>
         Task SaveRegionAsync(Region region);
+
+        /// <summary>
+        /// Edit <see cref="Region"/> entity
+        /// </summary>
+        /// <param name="region"></param>
+        /// <returns></returns>
+        Task<bool> UpdateRegionAsync(Region region);
+
+        /// <summary>
+        /// Add <see cref="City"/> to database
+        /// </summary>
+        /// <param name="city"></param>
+        /// <returns></returns>
+        Task SaveCityAsync(City city);
+
+        /// <summary>
+        /// Edit <see cref="City"/> entity
+        /// </summary>
+        /// <param name="city"></param>
+        /// <returns></returns>
+        Task<bool> UpdateCityAsync(City city);
     }
 }
